419d2216250d1a69131fa8c1af4116ea11938902
Mike Mannix <mike.mannix@gmail.com>
2016-02-12 16:40:10 -0500

    Extend calculator to two hours
    Still only handling before bedtime calculations. Introduced hours
    parameter for dynamic calculation.

--- charge-calculator.js
+++ charge-calculator.js
@@ -1,7 +1,7 @@
 var ChargeCalculator = function() {};
 
-ChargeCalculator.prototype.calculate = function() {
-  return 12;
+ChargeCalculator.prototype.calculate = function(hours) {
+  return hours * 12;
 };
 
 module.exports = ChargeCalculator;
--- spec/charge-calculator.spec.js
+++ spec/charge-calculator.spec.js
@@ -4,7 +4,11 @@ describe('Charge Calculator', function() {
   var chargeCalculator = new ChargeCalculator();
 
   it('calculates total charge for 1 hour before bedtime', function() {
-    expect(chargeCalculator.calculate()).toEqual(12.0);
+    expect(chargeCalculator.calculate(1)).toEqual(12.0);
+  });
+
+  it('calculates total charge for 2 hours before bedtime', function() {
+    expect(chargeCalculator.calculate(2)).toEqual(24.0);
   });
 
 });
