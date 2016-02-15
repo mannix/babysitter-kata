318468ed7330975d07703ff12d187d39bdfec6fd
Mike Mannix <mike.mannix@gmail.com>
2016-02-14 22:04:39 -0500

    More consistent error messages

--- charge-calculator.js
+++ charge-calculator.js
@@ -10,11 +10,11 @@ var validateTimeParameters = function(startTime, endTime) {
   }
 
   if (startTime.getHours() < 17) {
-    throw new Error("Start time can be no earlier than 5:00 PM");
+    throw new Error("Start time can not be earlier than 5:00 PM");
   }
 
   if (endTime.getHours() > 4 && endTime.getHours() <= 17) {
-    throw new Error("End time can not be after 4am");
+    throw new Error("End time can not be after 4:00 AM");
   }
 }
 
